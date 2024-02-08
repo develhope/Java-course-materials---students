a3df429b10ff7a15faaaf82cd5daaa6be85ec623
Sasa Sekulic <ja@sasasekulic.info>
2024-02-08 17:32:24 +0100

    removed DTO validation from swagger example

--- examples/5-spring-boot-intro/java16-java23/controller-with-swagger/src/main/java/com/example/demo/CreateBookRequestDto.java
+++ examples/5-spring-boot-intro/java16-java23/controller-with-swagger/src/main/java/com/example/demo/CreateBookRequestDto.java
@@ -6,16 +6,12 @@ import jakarta.validation.constraints.Size;
 
 public class CreateBookRequestDto {
 
-    @NotEmpty
-    @Size(min = 25)
     @Schema(description = "Book description", name = "description", type = "string", example = "Best book description ever!")
     String description;
     public String getDescription() {
         return description;
     }
 
-    @NotEmpty
-    @Size(min = 5)
     @Schema(description = "Book name", name = "name", type = "string", example = "Best book name ever!")
     String name;
 
